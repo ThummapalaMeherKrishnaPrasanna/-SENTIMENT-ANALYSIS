9721b6177936248f310cd90a52689aaac059d559
ThummapalaMeherKrishnaPrasanna <tmeherkrishna2004@gmail.com>
2025-03-12 16:40:27 +0530

    Add files via upload

---
+++ Sentiment Analysis.py
@@ -0,0 +1,17 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+
+nltk.download('vader_lexicon')
+
+# Initialize sentiment analyzer
+sia = SentimentIntensityAnalyzer()
+
+# Sample text
+text = "I absolutely love this product! It's amazing and works perfectly."
+
+
+
+# Perform sentiment analysis
+sentiment_score = sia.polarity_scores(text)
+print(sentiment_score)
